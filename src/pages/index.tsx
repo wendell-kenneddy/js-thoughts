27074254258681df2cb1cdd9f8d1a22d6535fc15
Wendell Kenneddy <wkenneddy505@gmail.com>
2021-12-29 20:15:49 -0300

    feat(homepage): add fake posts to test the page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,36 @@ import Head from "next/head";
 import { Flex } from "@chakra-ui/react";
 
 import { Header } from "../components/Header";
+const FAKE_POSTS = [
+  {
+    slug: "recriando-map-reduce-e-post",
+    title: "Recriando map, reduce e post",
+    description: "Some super fake description",
+    publicationDate: "08 dez 2021",
+    readTime: "8min",
+  },
+  {
+    slug: "testes-com-jest",
+    title: "Testes com Jest",
+    description: "Some super fake description",
+    publicationDate: "16 dez 2021",
+    readTime: "16min",
+  },
+  {
+    slug: "migrando-class-components-para-functional-components",
+    title: "Migrando Class Components para Functional Components",
+    description: "Some super fake description",
+    publicationDate: "30 dez 2021",
+    readTime: "32min",
+  },
+  {
+    slug: "criando-um-blog-com-nextjs-e-prismic",
+    title: "Criando um blog com NextJS e Prismic",
+    description: "Some super fake description",
+    publicationDate: "30 dez 2021",
+    readTime: "64min",
+  },
+];
 
 export default function Home() {
   return (
