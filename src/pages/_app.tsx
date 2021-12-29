27030815509b87739aeae86a7f5f55305492a459
Wendell Kenneddy <wkenneddy505@gmail.com>
2021-12-29 20:48:24 -0300

    feat: add header and footer to pages layout

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,18 @@
-import type { AppProps } from 'next/app';
+import type { AppProps } from "next/app";
 
-import { ChakraProvider } from '@chakra-ui/react';
+import { ChakraProvider } from "@chakra-ui/react";
 
-import { theme } from '../styles/theme';
+import { theme } from "../styles/theme";
+
+import { Header } from "../components/Header";
+import { Footer } from "../components/Footer";
 
 function App({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
+      <Header />
       <Component {...pageProps} />
+      <Footer />
     </ChakraProvider>
   );
 }
