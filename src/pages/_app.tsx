4e11790e258a8c375bb67d436fb5b684abfec985
Wendell Kenneddy <wkenneddy505@gmail.com>
2021-12-30 10:24:55 -0300

    feat: wrap application in QueryClientProvider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,17 +2,23 @@ import type { AppProps } from "next/app";
 
 import { ChakraProvider } from "@chakra-ui/react";
 
+import { QueryClient, QueryClientProvider } from "react-query";
+
 import { theme } from "../styles/theme";
 
 import { Header } from "../components/Header";
 import { Footer } from "../components/Footer";
 
 function App({ Component, pageProps }: AppProps) {
+  const queryClient = new QueryClient();
+
   return (
     <ChakraProvider theme={theme}>
-      <Header />
-      <Component {...pageProps} />
-      <Footer />
+      <QueryClientProvider client={queryClient}>
+        <Header />
+        <Component {...pageProps} />
+        <Footer />
+      </QueryClientProvider>
     </ChakraProvider>
   );
 }
