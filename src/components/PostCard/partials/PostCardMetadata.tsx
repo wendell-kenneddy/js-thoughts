196cbee8f525785dacad7acd994ecf27d7dceead
Wendell Kenneddy <wkenneddy505@gmail.com>
2021-12-30 16:38:03 -0300

    feat(postcardmetadata): update component to show last edited date

--- src/components/PostCard/partials/PostCardMetadata.tsx
+++ src/components/PostCard/partials/PostCardMetadata.tsx
@@ -4,17 +4,20 @@ import {
   Icon,
   useColorModeValue,
   StackProps,
+  Text,
 } from "@chakra-ui/react";
 
-import { FiCalendar, FiClock } from "react-icons/fi";
+import { FiCalendar, FiClock, FiEdit3 } from "react-icons/fi";
 
 interface PostCardMetadataProps extends StackProps {
   publcationDate: string;
+  lastEditedDate?: string;
   readTime: string;
 }
 
 export function PostCardMetadata({
   publcationDate,
+  lastEditedDate,
   readTime,
   ...rest
 }: PostCardMetadataProps) {
@@ -22,13 +25,21 @@ export function PostCardMetadata({
     <HStack
       color={useColorModeValue("gray.900", "whiteAlpha.700")}
       spacing="4"
-      fontSize={{ base: "sm", md: "md" }}
+      fontSize={{ base: "xs", md: "md" }}
+      flexWrap="wrap"
       {...rest}
     >
       <Flex align="center">
         <Icon as={FiCalendar} mr="2" />
         {publcationDate}
       </Flex>
+
+      {lastEditedDate && lastEditedDate !== publcationDate && (
+        <Flex align="center">
+          <Icon as={FiEdit3} mr="2" />
+          {lastEditedDate}
+        </Flex>
+      )}
 
       <Flex align="center">
         <Icon as={FiClock} mr="2" />
