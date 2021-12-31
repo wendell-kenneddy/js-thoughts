0929e1e8338d4e8b63e95e0d9afd30f1c7772221
Wendell Kenneddy <wkenneddy505@gmail.com>
2021-12-31 07:55:59 -0300

    feat: create getPreviousPost function

---
+++ src/lib/getPreviousPost.ts
@@ -0,0 +1,21 @@
+import Prismic from "@prismicio/client";
+import { getPrismicClient } from "../services/getPrismicClient";
+
+export async function getPreviousPost(after: string) {
+  const prismic = getPrismicClient();
+  const response = await prismic.query(
+    Prismic.predicates.at("document.type", "post"),
+    {
+      pageSize: 1,
+      after,
+      orderings: "[document.first_publication_date desc]",
+    }
+  );
+
+  if (!response.results[0]) return null;
+
+  return {
+    slug: response.results[0].uid as string,
+    title: response.results[0].data.title as string,
+  };
+}
