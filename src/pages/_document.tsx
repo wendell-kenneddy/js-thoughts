309af88c935567bbe571ae8a6badcf1e9924e638
Wendell Kenneddy <wkenneddy505@gmail.com>
2021-12-31 08:58:56 -0300

    feat(_document.tsx): add theme-color meta tags and initial color mode for chakra

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -17,12 +17,23 @@ export default class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap"
             rel="stylesheet"
           />
+          <meta
+            name="theme-color"
+            media="(prefers-color-scheme: dark)"
+            content="#1a202c"
+          />
+          <meta
+            name="theme-color"
+            media="(prefers-color-scheme: light)"
+            content="#fff"
+          />
           <link rel="icon" href="/favicon.ico" sizes="any" />
           <link rel="icon" href="/icon.svg" type="image/svg+xml" />
           <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+          <link rel="manifest" href="/manifest.json" />
         </Head>
         <body>
-          <ColorModeScript />
+          <ColorModeScript initialColorMode="system" />
           <Main />
           <NextScript />
         </body>
